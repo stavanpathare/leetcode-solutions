025c449f2370d15958915e7679d83ef22f0e27d5
Stavan Pathare <stavanpathare@gmail.com>
2026-06-29 22:09:28 +0530

    Time: 2 ms (46.01%), Space: 8.7 MB (90.13%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -0,0 +1,12 @@
+bool isPalindrome(int x) {
+    if(x < 0)
+        return false;
+    int original = x;
+    long reverse = 0;
+
+    while (x > 0) {
+        reverse =  reverse * 10 + x % 10;
+        x /= 10;
+    }
+    return original == reverse;
+}
